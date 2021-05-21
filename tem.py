5d60f7bed30ed24f2f47a4fe905b6a3dae5a46ed
ryan <1669971502@qq.com>
2021-05-21 18:32:46 +0800

    xiufu

--- tem.py
+++ tem.py
@@ -1,35 +1,25 @@
-from schema import Schema, And, Use, Optional, Regex
+from schema import Schema, And, Use, Optional, Regex, SchemaError
 
 data = {
-    'username': 'test001',
+    'username': 'tes',
     'password': '123456',
     'email': '1669971502@qq.com',
-    'nick_name': 'name'
-    # 'age': 18,
-    # 'school': [
-    #     {
-    #         'begin_time': '2013-09-01',
-    #         'end_time': '2015-06-30',
-    #         'name': 'XX高中'
-    #     },
-    #     {
-    #         'begin_time': '2015-09-01',
-    #         'end_time': '2019-06-30',
-    #         'name': 'XX大学'
-    #     }
-    # ]
+    'nick_name': '12121'
 }
 
-data1 = {'username': '1'}
 
 schema = {
-    'username': And(And(str, error='用户名参数字符类型错误'), And(lambda x: 4 <= x <= 16, error='用户名字符长度错误')),
-    'password': And(And(str, error='密码参数字符类型错误'), And(lambda x: 4 <= x <= 16, error='密码字符长度错误')),
+    'username': And(And(str, error='用户名参数字符类型错误'), And(lambda x: 4 <= len(x) <= 16, error='用户名字符长度错误')),
+    'password': And(And(str, error='密码参数字符类型错误'), And(lambda x: 4 <= len(x) <= 16, error='密码字符长度错误')),
     'email': And(And(str, error='邮箱参数字符类型错误'),
                  And(Regex(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$', error='邮箱正则匹配错误'))),
-    Optional('nick_name'): And(And(str, error='昵称参数字符类型错误'), And(lambda x: x <= 64, error='昵称字符长度错误'))
+    Optional('nick_name'): And(And(str, error='昵称参数字符类型错误'), And(lambda x: len(x) <= 64, error='昵称字符长度错误'))
 }
 
-result = Schema(schema).validate(data)
-print(result)
+
+try:
+    result = Schema(schema).validate(data)
+    print(result)
+except SchemaError as e:
+    print(e.code)
 
